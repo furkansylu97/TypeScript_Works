a433b346ee24de09eb70ef0612f586e2c9a8e681
Furkan Soylu <furkansylu97@gmail.com>
2025-05-02 21:42:27 +0300

    Data annotation work performed

--- 1-Setup/app.ts
+++ 1-Setup/app.ts
@@ -1,2 +1,8 @@
-console.log('TypeScript works were begun');
-console.log('Furkan Soylu');
+let age: number = 29;
+let firstname: string = "Furkan";
+let lastname: string = "Soylu";
+let isUpdated: boolean = false;
+
+function display(id:any , name:string) {
+    console.log("Id = " + id + ", Name = " + name )
+}
