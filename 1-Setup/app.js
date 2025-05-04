ab3fc22ffab5833417bd097985686048c74a250c
Furkan Soylu <furkansylu97@gmail.com>
2025-05-04 16:32:16 +0300

    Generic usage works were performed in class

--- 1-Setup/app.js
+++ 1-Setup/app.js
@@ -547,3 +547,42 @@ var list = new List();
 for (var i = 0; i < 7; i++) {
     list.add(i);
 }
+// GENERICS USAGE IN CLASS
+var Stack = /** @class */ (function () {
+    function Stack(size) {
+        this.size = size;
+        this.elements = [];
+    }
+    Stack.prototype.isEmpty = function () {
+        return this.elements.length === 0;
+    };
+    Stack.prototype.isFull = function () {
+        return this.elements.length === this.size;
+    };
+    Stack.prototype.push = function (element) {
+        if (this.elements.length === this.size) {
+            throw new Error('The stack is overflow!');
+        }
+        this.elements.push(element);
+    };
+    Stack.prototype.pop = function () {
+        if (this.elements.length == 0) {
+            throw new Error('The stack is empty!');
+        }
+        return this.elements.pop();
+    };
+    return Stack;
+}());
+function randBetween(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+var StackFuncnumbers = new Stack(5);
+while (!StackFuncnumbers.isFull()) {
+    var n = randBetween(1, 10);
+    console.log("Push ".concat(n, " into the stack."));
+    StackFuncnumbers.push(n);
+}
+while (!StackFuncnumbers.isEmpty()) {
+    var n = StackFuncnumbers.pop();
+    console.log("Pop ".concat(n, " from the stack."));
+}
--- 1-Setup/app.ts
+++ 1-Setup/app.ts
@@ -824,3 +824,47 @@ let list = new List<number>();
 for (let i = 0; i < 7; i++) {
     list.add(i);
 }
+
+// GENERICS USAGE IN CLASS
+
+class Stack<T> {
+    private elements: T[] = [];
+
+    constructor(private size: number) {
+    }
+    isEmpty(): boolean {
+        return this.elements.length === 0;
+    }
+    isFull(): boolean {
+        return this.elements.length === this.size;
+    }
+    push(element: T): void {
+        if (this.elements.length === this.size) {
+            throw new Error('The stack is overflow!');
+        }
+        this.elements.push(element);
+
+    }
+    pop(): T {
+        if (this.elements.length == 0) {
+            throw new Error('The stack is empty!');
+        }
+        return this.elements.pop()!;
+    }
+}
+
+function randBetween(low: number, high: number): number {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+let StackFuncnumbers = new Stack<number>(5);
+
+while (!StackFuncnumbers.isFull()) {
+    let n = randBetween(1, 10);
+    console.log(`Push ${n} into the stack.`)
+    StackFuncnumbers.push(n);
+}
+while (!StackFuncnumbers.isEmpty()) {
+    let n = StackFuncnumbers.pop();
+    console.log(`Pop ${n} from the stack.`);
+}
