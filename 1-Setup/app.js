bcbfb0441a7abdfe6f46c18a9c64020371622042
Furkan Soylu <furkansylu97@gmail.com>
2025-05-04 16:13:32 +0300

    Generic usage works were performed in interfaces

--- 1-Setup/app.js
+++ 1-Setup/app.js
@@ -522,3 +522,28 @@ var personal = merge({ name: "Furkan" }, { age: 27 }
     // 29 error
 );
 console.log(personal);
+var month = {
+    key: 1,
+    value: 'January'
+};
+console.log(month);
+var List = /** @class */ (function () {
+    function List() {
+        this.items = [];
+    }
+    List.prototype.add = function (o) {
+        this.items.push(o);
+        console.log(this.items);
+    };
+    List.prototype.remove = function (o) {
+        var index = this.items.indexOf(o);
+        if (index > -1) {
+            this.items.splice(index, 1);
+        }
+    };
+    return List;
+}());
+var list = new List();
+for (var i = 0; i < 7; i++) {
+    list.add(i);
+}
--- 1-Setup/app.ts
+++ 1-Setup/app.ts
@@ -783,3 +783,44 @@ let personal = merge(
 )
 
 console.log(personal);
+
+// GENERICS USAGE IN INTERFACE
+
+interface Months<U, V> {
+    key: U,
+    value: V
+}
+
+let month: Months<number, string> = {
+    key: 1,
+    value: 'January'
+}
+
+console.log(month);
+
+interface Collection<T> {
+    add(o: T): void;
+    remove(o: T): void;
+}
+
+class List<T> implements Collection<T>{
+    private items: T[] = [];
+    add(o: T): void {
+        this.items.push(o);
+        console.log(this.items);
+    }
+
+
+    remove(o: T): void {
+        let index = this.items.indexOf(o);
+        if (index > -1) {
+            this.items.splice(index, 1);
+        }
+    }
+}
+
+let list = new List<number>();
+
+for (let i = 0; i < 7; i++) {
+    list.add(i);
+}
