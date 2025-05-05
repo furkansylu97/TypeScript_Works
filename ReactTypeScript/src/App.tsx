727f666a1d33d1ce2fa333f14691faf6e0d84556
Furkan Soylu <furkansylu97@gmail.com>
2025-05-05 15:00:59 +0300

    Instructor.tsx components was created

--- ReactTypeScript/src/App.tsx
+++ ReactTypeScript/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Home from './components/Home'
+import Instructor from './components/Instructor'
 
 function App() {
 
   return (
     <>
       <Home name = 'Furkan Soylu' courseNumber = {15} isBest={true} />
+      <Instructor />
     </>
   )
 }
