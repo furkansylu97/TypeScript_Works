7bfd415310aa692d6b81ccbb5ed084967268bd43
Furkan Soylu <furkansylu97@gmail.com>
2025-05-05 17:31:21 +0300

    ReducerExamples constents were written

--- ReactTypeScript/src/components/ReducerExample.tsx
+++ ReactTypeScript/src/components/ReducerExample.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useReducer } from 'react';
+    
+  const initialState = { count: 0 };
+
+function reducer(state, action) {
+    switch (action.type) {
+      case 'increment':
+        return { count: state.count + action.payload };
+      case 'decrement':
+        return { count: state.count - action.payload };
+      case 'reset':
+        return initialState;
+      default:
+        return state;
+    }
+  }
 
 function ReducerExample() {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div>ReducerExample</div>
+    <>
+      Number: {state.count}
+      <button onClick={() => dispatch({ type: 'increment', payload: 15 })}>
+        15 İncrease
+      </button>
+      <button onClick={() => dispatch({ type: 'decrement', payload: 15 })}>
+        15 Decrease
+      </button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+    </>
   )
 }
 
