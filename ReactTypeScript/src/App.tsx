14094b4332c5f65dbace1c072c5fca547be02a14
Furkan Soylu <furkansylu97@gmail.com>
2025-05-05 16:08:32 +0300

    Request.tsx component was created and props works were performed

--- ReactTypeScript/src/App.tsx
+++ ReactTypeScript/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Home from './components/Home'
 import Instructor from './components/Instructor'
 import InstructorList from './components/InstructorList'
+import Request from './components/Request'
 
 function App() {
 
@@ -30,6 +31,7 @@ function App() {
       <Home name = 'Furkan Soylu' courseNumber = {15} isBest={true} />
       <Instructor instructureInfo = {instructer} />
       <InstructorList instructureListInfo = {instructerList} />
+      <Request status = 'loading' />
     </>
   )
 }
---
+++ ReactTypeScript/src/components/Request.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+type RequestProps = {
+    status: string;
+}
+
+function Request(props: RequestProps) {
+ 
+    let message = '';
+    if (props.status === 'loading') {
+        message = 'Loading..';
+    }
+    if (props.status === 'success') {
+        message = 'Successful..';
+    }
+    if (props.status === 'error') {
+        message = 'Error..';
+    }
+
+  return (
+    <div>
+        {message}
+    </div>
+  )
+}
+
+export default Request
