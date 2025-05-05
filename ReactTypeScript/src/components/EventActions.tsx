78df40de4a2d2fa2780e64ac08d68e6435b27a02
Furkan Soylu <furkansylu97@gmail.com>
2025-05-05 16:30:20 +0300

    onChange event definition and type definition process were performed

--- ReactTypeScript/src/components/EventActions.tsx
+++ ReactTypeScript/src/components/EventActions.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
 
 function EventActions() {
+
+    const [value, setvValue] = useState('');
 
     const handleClick = (event:React.MouseEvent<HTMLButtonElement>, id:number) => {
 
     };
 
+    const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+        setvValue(event.target.value)
+    };
+
   return (
     <div>
         <button onClick={(event) => handleClick(event, 4)}>Delete</button>
+        <input type="text" value={value} onChange={handleChange} />
     </div>
   )
 }
